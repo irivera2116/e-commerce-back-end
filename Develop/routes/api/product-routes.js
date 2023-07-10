3f09a1d7bd011097d41cb822783500ac4bb016e7
irivera2116 <irivera2116@GMAIL.COM>
2023-07-10 14:14:11 -0400

    added product options finding, creating and deleting

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    inlcude: [Category,
+    {
+      model: Tag,
+      through: ProductTag
+    }],
+  }).then(allProducts => res.json(allProducts)).catch(err => res.status(450).json(err));
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category,
+    {
+      model: Tag,
+      through: ProductTag
+    }],
+  }).then(ProductTag => res.json(ProductTag)).catch(err => res.status(450).json(err));
 });
 
 // create new product
@@ -94,6 +111,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    },
+  }).then(deletedProduct => res.json(deletedProduct)).catch(err => res.status(450).json(err));
 });
 
 module.exports = router;
